8d7940c10205dc9a416351af0052154208328a65
BenjieOK <Bkwasiowusu@gmail.com>
2022-06-27 01:21:20 +0000

    implement opCode using the switch statement

--- src/com/benjieok/calcengine/Main.java
+++ src/com/benjieok/calcengine/Main.java
@@ -6,15 +6,20 @@ public class Main {
         //this code performs operations based on the opCode selected
 
         double value1 = 100.0d;
-        double value2 = 50.0d;
+        double value2 = 0.0d;
         double result = 0.0d;
-        char opCode = 'a';
+        char opCode = 'd';
 
         if (opCode == 'a') result = value1 + value2;
         else if (opCode == 's') result = value1 - value2;
         else if (opCode == 'm') result = value1 * value2;
-        else if (opCode == 'd') result = value1 / value2;
-        else result = 0.0d;
+        else if (opCode == 'd'){
+            if (value2 != 0) result = value1 / value2;
+        }
+        else {
+            System.out.println("invalid opCode: "+ opCode);
+            result = 0.0d;
+        }
 
         System.out.println(result);
     }
