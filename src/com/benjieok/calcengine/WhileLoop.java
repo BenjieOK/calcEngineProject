512946f24f569e835d23184f6c2002904102490b
BenjieOK <Bkwasiowusu@gmail.com>
2022-06-27 01:52:35 +0000

    finding the factorial of number using while loop

---
+++ src/com/benjieok/calcengine/WhileLoop.java
@@ -0,0 +1,25 @@
+package com.benjieok.calcengine;
+
+public class WhileLoop {
+
+    public static void main(String[] args) {
+        /*
+         *What is factorial?
+         * The product of all positive integers less than or equal to a given positive integer.
+         * Thus, factorial seven is written 7!, meaning 1 × 2 × 3 × 4 × 5 × 6 × 7.
+         * Factorial zero is defined as equal to 1.
+         */
+
+        //finding the factorial of 5
+        int someValue = 5;
+        int factorial = 1;
+
+        while(someValue > 1) {
+            factorial *= someValue;
+            someValue--;
+        }
+
+        System.out.println(factorial);
+    }
+
+}
